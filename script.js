c6c060fd3d59aa926b3e652d6354e751f65113b9
MartaCanino <martacaninoromero@gmail.com>
2023-10-22 23:52:44 +0200

    Mensajes de error validacion

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// script.js
 $(document).ready(function () {
     $("#registerButton").click(function () {
         $("#registrationForm").fadeIn();
@@ -15,28 +16,42 @@ $(document).ready(function () {
         const phone = $("#phone").val();
         const password = $("#password").val();
 
+        // Reset error messages
+        $(".error-message").text("");
+
         // Name and surname validation
         const nameRegex = /^[\p{L}]+([\s][\p{L}]+)+$/u;
         const isNameValid = nameRegex.test(name);
+        if (!isNameValid) {
+            $("#nameError").text("Name and surname must contain at least two words, with letters only.");
+        }
 
         // Email validation
         const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
         const isEmailValid = emailRegex.test(email);
+        if (!isEmailValid) {
+            $("#emailError").text("Please enter a valid email address.");
+        }
 
         // Phone validation
         const phoneRegex = /^[0-9]+$/;
         const isPhoneValid = phoneRegex.test(phone);
+        if (!isPhoneValid) {
+            $("#phoneError").text("Phone must contain only numbers, with no spaces.");
+        }
 
         // Password validation
         const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
         const isPasswordValid = passwordRegex.test(password);
+        if (!isPasswordValid) {
+            $("#passwordError").text("Password must be at least 8 characters with one uppercase letter, one lowercase letter, and one number.");
+        }
 
         if (isNameValid && isEmailValid && isPhoneValid && isPasswordValid) {
-            $("#confirmationMessage").text("¡Usuario registrado con éxito!");
+            $("#confirmationMessage").text("Registration successful!");
             $("#confirmationMessage").show();
         } else {
-            $("#confirmationMessage").text("Registro fallido, compruebe los datos ingresados.");
-            $("#confirmationMessage").show();
+            $("#confirmationMessage").hide();
         }
     });
 });
